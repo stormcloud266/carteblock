8a4f3b1bacc1623e4a89f6138b4e3dc122804a14
Tawnee <allprt89@gmail.com>
2022-01-06 11:19:46 -0700

    add footer styles

--- src/components/global/footer/footer.js
+++ src/components/global/footer/footer.js
@@ -1,13 +1,54 @@
 import React from 'react'
-import { NavLinks } from '@global'
+import { Link } from 'gatsby'
+import { Container, Title } from '@UI'
+import { Logo } from '@images/icons'
 import * as styles from './footer.module.scss'
 
-const Footer = () => (
-	<footer className={styles.footer}>
-		<nav className={styles.nav}>
-			<NavLinks />
-		</nav>
-	</footer>
-)
+const Footer = () => {
+	const data = [
+		{
+			text: 'Email',
+			link: '',
+		},
+		{
+			text: 'LinkedIn',
+			link: '',
+		},
+		{
+			text: 'Telegram',
+			link: '',
+		},
+		{
+			text: 'Twitter',
+			link: '',
+		},
+	]
+
+	return (
+		<footer className={styles.footer}>
+			<Container wrapper section>
+				<div className={styles.row}>
+					<Link to='/' aria-label='return to top of the page'>
+						<Logo />
+					</Link>
+
+					<nav className={styles.nav}>
+						{data.map(({ text, link }) => (
+							<a href={link}>{text}</a>
+						))}
+					</nav>
+				</div>
+
+				<div className={styles.row}>
+					<div className={styles.smallText}>
+						<p>5 Shenton Way, #10-01</p>
+						<p>UIC Building, Singapore 068808</p>
+					</div>
+					<p className={styles.smallText}>&copy; Carteblock 2022</p>
+				</div>
+			</Container>
+		</footer>
+	)
+}
 
 export default Footer
