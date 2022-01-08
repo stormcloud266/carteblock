7ebf59d2e9c06ab0503bdd4ee054a16cf5fa7325
Tawnee <allprt89@gmail.com>
2022-01-08 16:31:31 -0700

    add clock and header styles

--- src/components/global/header/header.js
+++ src/components/global/header/header.js
@@ -1,12 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'gatsby'
+import Helmet from 'react-helmet'
 import classnames from 'classnames'
 import { Container } from '@UI'
+import { Logo } from '@images/icons'
 import * as styles from './header.module.scss'
-import Helmet from 'react-helmet'
 
 const Header = () => {
 	const [isLightTheme, setIsLightTheme] = useState(true)
+	const [time, setTime] = useState()
+
+	const getTime = () => {
+		const date = new Date().toLocaleString('en-GB', {
+			timeZone: 'Asia/Singapore',
+			hour: '2-digit',
+			minute: '2-digit',
+		})
+
+		return date
+	}
+
+	useEffect(() => {
+		setTime(getTime())
+
+		const interval = setInterval(() => {
+			setTime(getTime())
+		}, 1000)
+
+		return () => clearInterval(interval)
+	}, [])
 
 	return (
 		<header className={styles.header}>
@@ -17,24 +39,28 @@ const Header = () => {
 			/>
 			<Container wrapper className={styles.container}>
 				<Link to='/' className={styles.logo}>
-					Logo
+					<Logo />
 				</Link>
 
-				<div>
-					<button
-						className={styles.toggle}
-						onClick={() => setIsLightTheme(false)}
-						aria-label=''
-					>
-						night
-					</button>
+				<div className={styles.clockContainer}>
+					<time className={styles.clock}>{time} SNG</time>
+				</div>
 
+				<div>
 					<button
 						className={styles.toggle}
 						onClick={() => setIsLightTheme(true)}
 						aria-label=''
 					>
 						day
+					</button>
+
+					<button
+						className={styles.toggle}
+						onClick={() => setIsLightTheme(false)}
+						aria-label=''
+					>
+						night
 					</button>
 				</div>
 			</Container>
