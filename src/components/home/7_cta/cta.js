d4c8d0e0602340400d1a5a80a282014b9b805181
Tawnee <allprt89@gmail.com>
2022-01-15 17:24:25 -0700

    add image to cta

--- src/components/home/7_cta/cta.js
+++ src/components/home/7_cta/cta.js
@@ -1,15 +1,29 @@
 import React from 'react'
-import { Button, Container, Title } from '@UI'
+import { StaticImage } from 'gatsby-plugin-image'
+import { Button, Container, Flex, Title } from '@UI'
 import * as styles from './cta.module.scss'
 
 const Cta = () => {
 	return (
 		<Container wrapperSmOnMd isSection>
-			<Title size={2} className={styles.title}>
-				It is Carteblock’s mission to prepare business’ for the future and
-				uncover value in every step of the way forward...
-			</Title>
-			<Button href='mailto:info@carteblock.com'>Get In Touch</Button>
+			<Flex collapseOnMd reverseOnMd>
+				<div className={styles.textContainer}>
+					<Title size={2} className={styles.title}>
+						It is Carteblock’s mission to prepare business’ for the future and
+						uncover value in every step of the way forward...
+					</Title>
+					<Button href='mailto:info@carteblock.com'>Get In Touch</Button>
+				</div>
+
+				<div>
+					<StaticImage
+						src='../../../assets/images/PIXEL/ILLO1FINAL.png'
+						alt=''
+						placeholder='blurred'
+						loading='eager'
+					/>
+				</div>
+			</Flex>
 		</Container>
 	)
 }
