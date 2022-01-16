7d9e4ee539454e04cb8aa06b15456bf519364653
Tawnee <allprt89@gmail.com>
2022-01-16 15:37:59 -0700

    use benefit card for animation

---
+++ src/components/home/3_benefits/benefitCard.js
@@ -0,0 +1,52 @@
+import React, { useEffect } from 'react'
+import { motion, useAnimation } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
+import { Title } from '@UI'
+
+import * as styles from './benefits.module.scss'
+
+const BenefitCard = ({ number, title, description }) => {
+	const controls = useAnimation()
+
+	const variants = {
+		hidden: { opacity: 0 },
+		visible: { opacity: 1, transition: { duration: 1 } },
+	}
+
+	const inViewOptions = {
+		triggerOnce: false,
+		threshold: 0.3,
+	}
+
+	const [ref, inView] = useInView(inViewOptions)
+
+	useEffect(() => {
+		if (inView) {
+			controls.start('visible')
+		}
+		if (!inView) {
+			controls.start('hidden')
+		}
+	}, [controls, inView])
+
+	return (
+		<motion.div
+			className={styles.sectionContainer}
+			ref={ref}
+			variants={variants}
+			initial='hidden'
+			animate={controls}
+		>
+			{console.log('inView: ', inView)}
+			<Title size={3} tag='p' className={styles.label}>
+				{number}
+			</Title>
+			<Title tag='h3' size={2}>
+				{title}
+			</Title>
+			<p className={styles.description}>{description}</p>
+		</motion.div>
+	)
+}
+
+export default BenefitCard
--- src/components/home/3_benefits/benefits.js
+++ src/components/home/3_benefits/benefits.js
@@ -1,100 +1,23 @@
 import React from 'react'
-import { motion, useAnimation } from 'framer-motion'
-import { useInView } from 'react-intersection-observer'
-import { Container, Flex, Title } from '@UI'
+import { Container, Flex } from '@UI'
 import { benefits } from '@data'
 import * as styles from './benefits.module.scss'
+import BenefitCard from './benefitCard'
 
 const Benefits = () => {
-	const animationControl = useAnimation()
-	const animationControl2 = useAnimation()
-	const animationControl3 = useAnimation()
-
-	const inViewOptions = {
-		triggerOnce: false,
-		threshold: 0.2,
-	}
-
-	const animationOptions = {
-		opacity: 1,
-		transition: { duration: 1 },
-	}
-
-	const [ref, inView] = useInView(inViewOptions)
-	const [ref2, inView2] = useInView(inViewOptions)
-	const [ref3, inView3] = useInView(inViewOptions)
-
-	if (inView) {
-		animationControl.start(animationOptions)
-	}
-
-	if (inView2) {
-		animationControl2.start(animationOptions)
-	}
-
-	if (inView3) {
-		animationControl3.start(animationOptions)
-	}
-
 	return (
-		<div>
-			<motion.div>
-				<Container wrapperSmOnMd isSection className={styles.container}>
-					<Flex
-						alignStart
-						noHorizontalMargin
-						noCollapseMargin
-						collapseOnMd
-						className={styles.flex}
-					>
-						<motion.div
-							className={styles.sectionContainer}
-							ref={ref}
-							initial={{ opacity: 0 }}
-							animate={animationControl}
-						>
-							<Title size={3} tag='p' className={styles.label}>
-								01
-							</Title>
-							<Title tag='h3' size={2}>
-								{benefits[0].title}
-							</Title>
-							<p className={styles.description}>{benefits[0].description}</p>
-						</motion.div>
-
-						<motion.div
-							className={styles.sectionContainer}
-							ref={ref2}
-							initial={{ opacity: 0 }}
-							animate={animationControl2}
-						>
-							<Title size={3} tag='p' className={styles.label}>
-								02
-							</Title>
-							<Title tag='h3' size={2}>
-								{benefits[1].title}
-							</Title>
-							<p className={styles.description}>{benefits[1].description}</p>
-						</motion.div>
-
-						<motion.div
-							className={styles.sectionContainer}
-							ref={ref3}
-							initial={{ opacity: 0 }}
-							animate={animationControl3}
-						>
-							<Title size={3} tag='p' className={styles.label}>
-								03
-							</Title>
-							<Title tag='h3' size={2}>
-								{benefits[2].title}
-							</Title>
-							<p className={styles.description}>{benefits[2].description}</p>
-						</motion.div>
-					</Flex>
-				</Container>
-			</motion.div>
-		</div>
+		<Container wrapperSmOnMd isSection className={styles.container}>
+			<Flex alignStart noHorizontalMargin noCollapseMargin collapseOnMd>
+				{benefits.map(({ title, description }, index) => (
+					<BenefitCard
+						title={title}
+						description={description}
+						number={`0${index + 1}`}
+						key={title}
+					/>
+				))}
+			</Flex>
+		</Container>
 	)
 }
 
