83a0e76410aa31060ff3063b54b75a80d91c58b3
Tawnee <allprt89@gmail.com>
2023-09-07 15:48:21 -0600

    footer and meta description updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,50 +1,50 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-	siteMetadata: {
-		title: 'Carteblock',
-		siteUrl: `https://carteblock.com`,
-		description: `CarteBlock is a tech-focused management consultancy that is specialized in helping blockchain start-ups and companies scale and adapt to meet the challenges of the ever-changing markets.`,
-	},
-	plugins: [
-		'gatsby-plugin-sass',
-		'gatsby-plugin-image',
-		'gatsby-plugin-react-helmet',
-		'gatsby-plugin-sitemap',
-		'gatsby-plugin-dark-mode',
-		'gatsby-plugin-anchor-links',
-		{
-			resolve: 'gatsby-plugin-manifest',
-			options: {
-				icon: 'src/assets/images/favicon.png',
-			},
-		},
-		'gatsby-plugin-sharp',
-		'gatsby-transformer-sharp',
-		{
-			resolve: 'gatsby-source-filesystem',
-			options: {
-				name: 'images',
-				path: './src/assets/images/',
-			},
-			__key: 'images',
-		},
-		{
-			resolve: `gatsby-plugin-alias-imports`,
-			options: {
-				alias: {
-					'@components': path.resolve(__dirname, 'src/components'),
-					'@global': path.resolve(__dirname, 'src/components/global'),
-					'@animations': path.resolve(__dirname, 'src/components/animations'),
-					'@UI': path.resolve(__dirname, 'src/components/UI'),
-					'@home': path.resolve(__dirname, 'src/components/home'),
-					'@images': path.resolve(__dirname, 'src/assets/images'),
-					'@globalStyles': path.resolve(__dirname, 'src/assets/globalStyles'),
-					'@context': path.resolve(__dirname, 'src/context'),
-					'@hooks': path.resolve(__dirname, 'src/hooks'),
-					'@data': path.resolve(__dirname, 'src/data'),
-				},
-			},
-		},
-	],
-}
+  siteMetadata: {
+    title: "Carteblock",
+    siteUrl: `https://carteblock.com`,
+    description: `Carteblock is a strategic management consultancy, guiding startups and enterprises with smart technology for growth in dynamic markets.`,
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-dark-mode",
+    "gatsby-plugin-anchor-links",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/assets/images/favicon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/assets/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: `gatsby-plugin-alias-imports`,
+      options: {
+        alias: {
+          "@components": path.resolve(__dirname, "src/components"),
+          "@global": path.resolve(__dirname, "src/components/global"),
+          "@animations": path.resolve(__dirname, "src/components/animations"),
+          "@UI": path.resolve(__dirname, "src/components/UI"),
+          "@home": path.resolve(__dirname, "src/components/home"),
+          "@images": path.resolve(__dirname, "src/assets/images"),
+          "@globalStyles": path.resolve(__dirname, "src/assets/globalStyles"),
+          "@context": path.resolve(__dirname, "src/context"),
+          "@hooks": path.resolve(__dirname, "src/hooks"),
+          "@data": path.resolve(__dirname, "src/data"),
+        },
+      },
+    },
+  ],
+};
--- src/components/global/footer/footer.js
+++ src/components/global/footer/footer.js
@@ -1,45 +1,46 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import { Container } from '@UI'
-import { Fade } from '@animations'
-import { Logo } from '@images/icons'
-import { footerLinks } from '@data'
-import * as styles from './footer.module.scss'
+import React from "react";
+import { Link } from "gatsby";
+import { Container } from "@UI";
+import { Fade } from "@animations";
+import { Logo } from "@images/icons";
+import { footerLinks } from "@data";
+import * as styles from "./footer.module.scss";
 
 const Footer = () => {
-	return (
-		<footer className={styles.footer}>
-			<Fade>
-				<Container wrapper className={styles.container}>
-					<div className={styles.row}>
-						<Link
-							to='/'
-							aria-label='return to top of the page'
-							className={styles.logo}
-						>
-							<Logo />
-						</Link>
+  return (
+    <footer className={styles.footer}>
+      <Fade>
+        <Container wrapper className={styles.container}>
+          <div className={styles.row}>
+            <Link
+              to="/"
+              aria-label="return to top of the page"
+              className={styles.logo}
+            >
+              <Logo />
+            </Link>
 
-						<nav className={styles.nav}>
-							{footerLinks.map(({ text, link }) => (
-								<a href={link} key={text} target='_blank' rel='noreferrer'>
-									{text}
-								</a>
-							))}
-						</nav>
-					</div>
+            <nav className={styles.nav}>
+              {footerLinks.map(({ text, link }) => (
+                <a href={link} key={text} target="_blank" rel="noreferrer">
+                  {text}
+                </a>
+              ))}
+            </nav>
+          </div>
 
-					<div className={styles.row}>
-						<div className={styles.smallText}>
-							<p>5 Shenton Way, #10-01</p>
-							<p>UIC Building, Singapore 068808</p>
-						</div>
-						<p className={styles.smallText}>&copy; Carteblock 2022</p>
-					</div>
-				</Container>
-			</Fade>
-		</footer>
-	)
-}
+          <div className={styles.row}>
+            <div className={styles.smallText}>
+              <a href="mailto:info@carteblock.com">info@carteblock.com</a>
+            </div>
+            <p className={styles.smallText}>
+              &copy; Carteblock {new Date().getFullYear()}
+            </p>
+          </div>
+        </Container>
+      </Fade>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
